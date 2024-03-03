c80750bb6f536e563254c914990f90ffd8068023
JJ Asghar <awesome@ibm.com>
2024-03-03 00:02:37 -0600

    black and fixed typo

--- app.py
+++ app.py
@@ -1,26 +1,31 @@
-from flask import Flask, request,jsonify,render_template
+from flask import Flask, request, jsonify, render_template
 from github import Github
 from dotenv import load_dotenv
 import os
-if os.path.exists('.env'):
-    load_dotenv('.env')
+
+if os.path.exists(".env"):
+    load_dotenv(".env")
     token = os.getenv("TOKEN")
-    organization_name=os.getenv("ORGANIZATION_NAME")
-    redirection_url=os.getenv("REDIRECTION_URL")
+    organization_name = os.getenv("ORGANIZATION_NAME")
+    redirection_url = os.getenv("REDIRECTION_URL")
 else:
     token = os.environ.get("TOKEN")
-    organization_name=os.environ.get("ORGANIZATION_NAME")
-    redirection_url=os.environ.get("REDIRECTION_URL")
+    organization_name = os.environ.get("ORGANIZATION_NAME")
+    redirection_url = os.environ.get("REDIRECTION_URL")
 
-app=Flask(__name__)
-@app.route('/')
+app = Flask(__name__)
+
+
+@app.route("/")
 def index():
-    return render_template('register.html',organization_name=organization_name)
-@app.route('/invite',methods=['POST','GET'])
+    return render_template("register.html", organization_name=organization_name)
+
+
+@app.route("/invite", methods=["POST", "GET"])
 def invite():
-    if(request.method=='GET'):
-        email=request.args.get('git_email')
-        password=request.argoeus.get('password')
+    if request.method == "GET":
+        email = request.args.get("git_email")
+        password = request.args.get("password")
         try:
             if password == os.environ.get("PASSWORD"):
                 gh = Github(token)
@@ -28,15 +33,17 @@ def invite():
 
                 try:
                     org.invite_user(email=email)
-                    return render_template('success.html',redirection_url=redirection_url)
-                except :
-                    return render_template('unsuccess.html')
+                    return render_template(
+                        "success.html", redirection_url=redirection_url
+                    )
+                except:
+                    return render_template("unsuccess.html")
             else:
-                return '<h1>Oops ! Wrong password</h1><hr><p>Please update it or reach out in our slack channel.</p>'
+                return "<h1>Oops ! Wrong password</h1><hr><p>Please update it or reach out in our slack channel.</p>"
+
+        except:
+            return "<h1>Oops ! Invalid Token ID or Organization Name</h1><hr><p>Please Modify it from App Settings->Config Vars option or <i>Create a new app</i></p>"
 
 
-        except :
-            return '<h1>Oops ! Invalid Token ID or Organization Name</h1><hr><p>Please Modify it from App Settings->Config Vars option or <i>Create a new app</i></p>'
-
-if __name__== '__main__':
+if __name__ == "__main__":
     app.run(debug=False)
